6945c5893c75d41c0f2a3cf1d527a1ca68711785
Gabriel Galves Taliatti <galvesmash.dev@gmail.com>
2022-04-22 21:14:22 -0300

    feat: starting project, added favicon and logo.

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+    >
+      Header
+    </v-app-bar>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    name: "App",
+    data: () => ({
+      //
+    }),
+  };
+</script>
